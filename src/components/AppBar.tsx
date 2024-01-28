e533353c37f607bd4daac196f30bc401a3efbb4e
Vlad Goloyan <vladimir.goloyan@helpsystems.com>
2024-01-28 18:06:20 +0400

    Implemented basic google authentication for aua.am organization users

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SignInButton from "@/components/SignInButton";
+
+const AppBar = () => {
+    return <header>
+        <SignInButton/>
+    </header>
+}
+
+export default AppBar;
